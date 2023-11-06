0bef13f51c4d1ce4106caf759db219a7d434d9de
Zzang-yeah <71954155+Zzang-yeah@users.noreply.github.com>
2023-11-06 23:43:17 +0900

    회원가입페이지, 비밀번호찾기 페이지, 화면이동 구현
    회원가입페이지, 비밀번호찾기 페이지, 화면이동 구현

--- home/lib/main.dart
+++ home/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:home/styles/app_colors.dart';
-
+import 'package:home/view/LoginPage/loginPage.dart';
+import 'package:home/view/LoginPage/splashPage.dart';
 import 'package:home/view/homePage/main_page.dart';
+import 'package:home/view/talkPage/TalkPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,14 +14,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: AppColors.stroke_line_5,
-        fontFamily: 'Pretendard',
+    return SafeArea(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/' : (context) => SplashPage(),
+          '/login' : (context) => const LoginPage(),
+          '/home' : (context) => const MainPage(),
+          '/talk' : (context) => const TalkPage(),
+        },
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          fontFamily: 'Pretendard',
+        ),
+        // home: UpdatePage(),
+        // home: Scaffold(
+        //     body: SplashPage())
       ),
-      // home: UpdatePage(),
-      // home: SplashPage(),
     );
   }
 }

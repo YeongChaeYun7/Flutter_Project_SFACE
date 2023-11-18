d4c338b269038c60623ae4f28a97b1ce637ab890
Yeong Chae Yun <xerochae@gmail.com>
2023-11-18 14:46:24 +0900

    hompage 수정 및 test 페이지 미완성 팝업 연결 및 gestruedetector 추가

--- home/lib/view/catchupPage/ShimmerAllCatchupPage.dart
+++ home/lib/view/catchupPage/ShimmerAllCatchupPage.dart
@@ -1,53 +1,60 @@
+// 핫한 캐치업
 import 'package:flutter/material.dart';
-import 'package:home/styles/app_colors.dart';
-import 'package:home/widget/BottomNavBar/BottomNavBar.dart';
-
+import 'package:home/widget/Catchup/CatchupCard.dart';
+import 'package:home/widget/Catchup/TechnologyStackList.dart';
+import 'package:home/widget/Common/SortButton.dart';
+import 'package:home/widget/Common/TitleBar2.dart';
 import 'package:home/widget/Search/search.dart';
-import 'package:home/widget/Shimmer/ShimmerCatchup.dart';
-import 'package:home/widget/Shimmer/ShimmerContainer.dart';
 
-import 'package:shimmer/shimmer.dart';
-
-class ShimmerAllCatchupPage extends StatelessWidget {
-  const ShimmerAllCatchupPage({super.key});
+class HotCatchupPage extends StatelessWidget {
+  const HotCatchupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [
-        const Search(),
-        SizedBox(height: 20),
-        Shimmer.fromColors(
-          baseColor: AppColors.neutral_20,
-          highlightColor: Colors.white,
-          child: Column(children: [
-            ShimmerContainer(
-              height: 18,
-              width: MediaQuery.of(context).size.width * 0.95,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            const SizedBox(height: 3),
+            const Search(),
+            const TitleBar2(Title: '핫한 캐치업'),
+            const TechnologyStackList(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SortButton(),
+              ],
             ),
-            const SizedBox(height: 15),
-            const ShimmerCatchup(),
-            const SizedBox(height: 70),
-            ShimmerContainer(
-              height: 18,
-              width: MediaQuery.of(context).size.width * 0.95,
+            const SizedBox(height: 8),
+            CatchupCard(
+              imagePath: 'assets/icon/icon_70/laptop.svg',
+              userClass: '개발자/1기',
+              userName: '신디',
+              userType: '수료생',
             ),
-            const SizedBox(height: 15),
-            ListView.separated(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return const ShimmerCatchup();
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(height: 30);
-              },
-              itemCount: 3,
+            const SizedBox(height: 16),
+            CatchupCard(
+              imagePath: 'assets/icon/icon_70/laptop.svg',
+              userClass: '개발자/1기',
+              userName: '신디',
+              userType: '수료생',
             ),
-          ]),
+            CatchupCard(
+              imagePath: 'assets/icon/icon_70/laptop.svg',
+              userClass: '개발자/1기',
+              userName: '신디',
+              userType: '수료생',
+            ),
+            CatchupCard(
+              imagePath: 'assets/icon/icon_70/laptop.svg',
+              userClass: '개발자/1기',
+              userName: '신디',
+              userType: '수료생',
+            ),
+          ],
         ),
-      ]),
-      bottomNavigationBar: MyBottomNavigationBar(),
+      ),
     );
   }
 }
--- home/lib/view/catchupPage/ShimmerHotCatchupPage.dart
+++ home/lib/view/catchupPage/ShimmerHotCatchupPage.dart
@@ -1,70 +1,60 @@
+// 핫한 캐치업
 import 'package:flutter/material.dart';
-import 'package:home/styles/app_colors.dart';
-import 'package:home/widget/BottomNavBar/BottomNavBar.dart';
-
+import 'package:home/widget/Catchup/CatchupCard.dart';
+import 'package:home/widget/Catchup/TechnologyStackList.dart';
+import 'package:home/widget/Common/SortButton.dart';
+import 'package:home/widget/Common/TitleBar2.dart';
 import 'package:home/widget/Search/search.dart';
-import 'package:home/widget/Shimmer/ShimmerCatchup.dart';
-import 'package:home/widget/Shimmer/ShimmerContainer.dart';
-import 'package:home/widget/Shimmer/ShimmerRectangleContainer.dart';
 
-import 'package:shimmer/shimmer.dart';
-
-class ShimmerHotCatchupPage extends StatelessWidget {
-  const ShimmerHotCatchupPage({super.key});
+class HotCatchupPage extends StatelessWidget {
+  const HotCatchupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [
-        const Search(),
-        Shimmer.fromColors(
-          baseColor: AppColors.neutral_20,
-          highlightColor: Colors.white,
-          child: Column(children: [
-            const SizedBox(height: 20),
-            ShimmerContainer(
-              height: 18,
-              width: MediaQuery.of(context).size.width * 0.95,
-            ),
-            const SizedBox(height: 25),
-            const Row(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            const SizedBox(height: 3),
+            const Search(),
+            const TitleBar2(Title: '핫한 캐치업'),
+            const TechnologyStackList(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                SizedBox(
-                  width: 15,
-                ),
-                ShimmerRectangleContainer(),
-                SizedBox(
-                  width: 15,
-                ),
-                ShimmerRectangleContainer(),
-                SizedBox(
-                  width: 15,
-                ),
-                ShimmerRectangleContainer(),
-                SizedBox(
-                  width: 15,
-                ),
-                ShimmerRectangleContainer()
+                SortButton(),
               ],
             ),
-            SizedBox(
-              height: 70,
+            const SizedBox(height: 8),
+            CatchupCard(
+              imagePath: 'assets/icon/icon_70/laptop.svg',
+              userClass: '개발자/1기',
+              userName: '신디',
+              userType: '수료생',
             ),
-            ListView.separated(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return const ShimmerCatchup();
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(height: 40);
-              },
-              itemCount: 3,
+            const SizedBox(height: 16),
+            CatchupCard(
+              imagePath: 'assets/icon/icon_70/laptop.svg',
+              userClass: '개발자/1기',
+              userName: '신디',
+              userType: '수료생',
             ),
-          ]),
+            CatchupCard(
+              imagePath: 'assets/icon/icon_70/laptop.svg',
+              userClass: '개발자/1기',
+              userName: '신디',
+              userType: '수료생',
+            ),
+            CatchupCard(
+              imagePath: 'assets/icon/icon_70/laptop.svg',
+              userClass: '개발자/1기',
+              userName: '신디',
+              userType: '수료생',
+            ),
+          ],
         ),
-      ]),
-      bottomNavigationBar: MyBottomNavigationBar(),
+      ),
     );
   }
 }
